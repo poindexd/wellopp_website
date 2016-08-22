94b12e19e95a03d82f7a384a5950f90d5b85ad01
poindexd <danmanpoindexter@gmail.com>
2016-08-22 17:09:30 -0400

    Brought back stat charts

--- js/main.js
+++ js/main.js
@@ -33,11 +33,17 @@ function stats(){
 	$('.percentage').easyPieChart({
 		animate: 2000,
 		lineWidth: 4,
+		size: 120,
+		text: 0,
+		onStart: function(value) {
+			this.text = this.$el.data('text');
+			this.percent = this.$el.data('percent')
+		},
 		onStep: function(value) {
-			this.$el.find('span').text(Math.round(value)).digits();
+			this.$el.find('span').text(Math.round(this.text * (value/this.percent))).digits();
 		},
 		onStop: function(value, to) {
-			this.$el.find('span').text(Math.round(to)).digits();
+			this.$el.find('span').text(Math.round(this.text * (value/this.percent))).digits();
 		},
 		//trackColor:'#143f52',
 		//trackColor: '#ffffff00',
